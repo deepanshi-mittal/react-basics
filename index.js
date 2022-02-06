3a46c94c1882f8a0c36e4edcaca08bf77236c1db
Deepanshi Mittal <deepanshimittal1998@gmail.com>
2022-02-06 20:34:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+ReactDOM.render(<h1>Hello everyone!</h1>,document.getElementById("root"))
+/*render parameter : 1st parameter is html code that we want to render
+2nd parameter is the destination where we want to render our code */
